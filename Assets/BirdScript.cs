af5de23a8e38b5c77b0699f910a9a71c2f9fe628
Andrey <amalgota@mail.ru>
2022-12-09 12:59:20 +0300

    test commit
    test commit description

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // testing 1 2 3
+        // // testing 1 2 3    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
