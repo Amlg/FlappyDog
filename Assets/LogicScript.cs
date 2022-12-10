731e60c94c7863d17ce034965c5717d6f3dcfa8c
Andrey <amalgota@mail.ru>
2022-12-10 03:46:49 +0300

    collision+score+endgame screen

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int PlayerScore;
+    public Text scoretext;
+    public GameObject gameOverScreen;
+    [ContextMenu("Increase Score")]
+    public void addScore(int ScoreToAdd)
     {
+        PlayerScore = PlayerScore + ScoreToAdd;
+        scoretext.text = PlayerScore.ToString();
+
+    }
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+    public void gameOver() 
+    {
+        gameOverScreen.SetActive(true);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
